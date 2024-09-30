c37139b0d355fe8814c853639db952176cc979f0
Vybhav Achar Bhargav <vybhavab@glyf.space>
2024-09-29 20:58:16 -0700

    feat: init

---
+++ src/app/providers.tsx
@@ -0,0 +1,17 @@
+"use client";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: "/ingest",
+    ui_host: "https://us.posthog.com",
+    person_profiles: "identified_only",
+    capture_pageview: false,
+    debug: true,
+  });
+}
+
+export function PHProvider({ children }: { children: React.ReactNode }) {
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+}
